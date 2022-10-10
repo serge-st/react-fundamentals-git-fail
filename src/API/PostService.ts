d958bae788cc51f9c3f94a3e14705c7f6714f93b
Serge Stecenko <sergejs.stecenko@gmail.com>
2022-10-10 15:27:49 +0300

    implement cacheing

--- src/API/PostService.ts
+++ src/API/PostService.ts
@@ -1,13 +1,67 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { cache } from './cacheHandler';
 
 export default class PostService {
+    private static responseHandler(response: AxiosResponse<any>): AxiosResponse<any> {
+        if (response.config.method === 'GET' || 'get') {
+            if (response.config.url && response.config.params) {
+                console.log('storing in cache');
+                console.log(response);
+                const key = response.config.url + Object
+                    .entries(response.config.params)
+                    .map( param => param.join('=')).join('&');
+                cache.store(
+                    key,
+                    JSON.stringify(response.data),
+                    JSON.stringify(response.headers)
+                );
+            }
+        }
+        return response;
+    }
+
+    private static errorHandler(error: any) {
+        if (error.headers.cached === true) {
+            console.log('got cached data in response, serving it directly');
+            return Promise.resolve(error);
+        }
+        return Promise.reject(error);
+    }
+    
+    private static requestHandler = (request: AxiosRequestConfig) => {
+        if (request.method === 'GET' || 'get') {
+            const key = request.url + Object
+                .entries(request.params)
+                .map( param => param.join('=')).join('&');
+            const checkIsValidResponse = cache.isValid(key);
+            if (checkIsValidResponse.isValid) {
+                console.log('serving cached data');
+                request.headers = JSON.parse(checkIsValidResponse.headers || '{}');
+                if (request.headers) {
+                    request.headers.cached = true;
+                }
+                request.data = JSON.parse(checkIsValidResponse.value || '{}');
+                return Promise.reject(request);
+            }
+        }
+        return request;
+    }
+
     static async getAll(limit = 10, page = 1) {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?', {
+        const client = axios.create({
+            baseURL: 'https://jsonplaceholder.typicode.com'
+        });
+
+        client.interceptors.request.use((request) => this.requestHandler(request));
+        client.interceptors.response.use(
+            (response) => this.responseHandler(response),
+            (error) => this.errorHandler(error),
+        );
+        return await client.get('/posts?', {
             params: {
                 _limit: limit,
                 _page: page,
             }
         });
-        return response;
     }
 }
---
+++ src/API/cacheHandler.ts
@@ -0,0 +1,50 @@
+interface IsValidResponse {
+    isValid: boolean,
+    value?: string,
+    headers?: string,
+}
+
+const SEPARATOR = '//**//';
+const CACHE_INTERVAL = 0.3 * 60 * 1000;
+
+const store = (key: string, value: string, headers: string) => {
+    const finalValue = `${value}${SEPARATOR}${headers}${SEPARATOR}${Date.now().toString()}`;
+    localStorage.setItem(key, finalValue);
+}
+
+const isValid = (key: string): IsValidResponse => {
+    const value = localStorage.getItem(key);
+    if (value === null) {
+        return {
+            isValid: false,
+        };
+    }
+    const values = value.split(SEPARATOR);
+    const timestamp = Number(values[2]);
+    if (Number.isNaN(timestamp)) {
+        return {
+            isValid: false,
+        };
+    }
+    const date = new Date(timestamp);
+    if (date.toString() === 'Invalid Date') {
+        return {
+            isValid: false,
+        };
+    }
+    if ((Date.now() - date.getTime()) < CACHE_INTERVAL) {
+        return {
+            isValid: true,
+            value: values[0],
+            headers: values[1],
+        };
+    }
+    localStorage.removeItem(key);
+    return {
+        isValid: false,
+    };
+}
+
+export const cache = {
+    store, isValid,
+};
